a5c458f15746171ffd56afc6d518649558af08a1
koroknaibotond <127404741+koroknaibotond@users.noreply.github.com>
2023-04-05 22:32:56 +0200

    Update NewtonRaphson.cpp

--- homework1/NewtonRaphson.cpp
+++ homework1/NewtonRaphson.cpp
@@ -4,7 +4,7 @@
 template <typename F, typename D, typename T> // creating the template of the Newton-Raphson iteration
 T Newton(F f, D df, T x0)
 {
-    int iter_num = 10; // number of repeats which i though would be more than enough to give a good result for the squareroot.
+    int iter_num = 10; // number of repeats which i though would be more than enough to give a good result for the square root.
     for (int i = 0; i < iter_num; ++i)
     {
         T x1 = x0 - f(x0) / df(x0);
@@ -20,5 +20,5 @@ int main()
                            [](double x)
                            { return 2.0 * x; },
                            10.0);
-    std::cout << "The squareroot of the function is: " << result << std::endl;
+    std::cout << "The square root of the function is: " << result << std::endl;
 }
