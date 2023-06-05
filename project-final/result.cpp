ce8ec8909e766cf3e542f47bd86ec9bd369886a4
koroknaibotond <botond.koroknai@gmail.com>
2023-06-05 10:39:09 +0200

    update

--- project-final/result.cpp
+++ project-final/result.cpp
@@ -14,16 +14,17 @@ int main()
     double result = params.getResult();
     std::cout << "\n";
 
-    std::cout << "Az integrálás eredménye: " << result << std::endl;
-    std::cout << "\n";
+    /* std::cout << "Az integrálás eredménye: " << result << std::endl;
+     std::cout << "\n";
+    */
 
     double control = params.getControl();
-    std::cout << "Az ellenőrzés eredménye: " << control << std::endl;
+    /* std::cout << "Az ellenőrzés eredménye: " << control << std::endl;
     std::cout << "\n";
-
-    double diff = pow(control - result, 2);
-    std::cout << "A két integrálás négyzetes eltérése:" << diff << std ::endl;
-
+    */
+    /* double diff = pow(control - result, 2);
+     std::cout << "A két integrálás négyzetes eltérése:" << diff << std ::endl;
+   */
     double percent = 100 * std::abs((result - control) / control);
     std::cout << "A két integrálás százalákos eltérése:" << percent << std ::endl;
     return 0;
