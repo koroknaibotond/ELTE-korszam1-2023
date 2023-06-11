84abaeab1c629154f858585ba8967b9521bb567b
koroknaibotond <127404741+koroknaibotond@users.noreply.github.com>
2023-06-11 20:24:00 +0200

    Project

--- project-final/montecarlo.hpp
+++ project-final/montecarlo.hpp
@@ -1,70 +1,77 @@
 #include <iostream>
 #include <random>
 #include <cmath>
-#include <functional>
 
 class MonteCarlo
 {
 private:
     std::random_device rd;
-    std::mt19937 gen;
+    std::mt19937 gen; // a randomszám generáláshoz egy Mersenne Twister típusú véletlenszám-generátort használtam
     std::uniform_real_distribution<double> distributionx;
-    std::uniform_real_distribution<double> distributiony;
+    std::uniform_real_distribution<double> distributiony; // A koordinátákhoz a megadott határok között egyenletesen elosztott véleletlenszámokat generáltam
     std::uniform_real_distribution<double> distributionz;
-    double result;
-    double controlvalue;
+    double result;       // Ebbe a változóban tároljuk az Montecarlo integrálás eredményét
+    double controlvalue; // Ebben pedig az ellenőrzés során kiszámolt értéket
 
 public:
     template <typename Func, typename Dom>
-    MonteCarlo(Func integrand, Dom domain, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax, int samples = 1e6)
+    MonteCarlo(Func integrand, Dom domain, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax, int samples = 0.5e6)
         : gen(rd()), distributionx(xmin, xmax), distributiony(ymin, ymax), distributionz(zmin, zmax)
-    {
-        result = integrate(integrand, domain, samples);
-        controlvalue = control(integrand);
+    { // Ebben a konstuktoban adhatjuk meg az integrandust, az integrálási tartományt, és a tartományt magába foglaló téglalap határait
+      // valamint inicializáljuk randomszám generátort, és az eloszlásokat a megfelelő határokkal
+        try
+        {
+            result = integrate(integrand, domain, samples); // Elvégezzük a Montecarlo integrálást
+            controlvalue = control(integrand, domain);      // Elvégezzük téglalap módszeres integrálást
+            double diff = pow(controlvalue - result, 2);    // Kiszámítjuk a négyzetes eltérést
+            if (diff > 1e-04)                               // Ha az eltérés meghaladja a hibahatárt.
+            {
+                throw std::runtime_error("Hiba! A kiszámított érték és a Control érték négyzetes eltérése nagyobb mint : 1e-04"); // Hibát kapunk
+            }
+        }
+        catch (const std::runtime_error &e)
+        {
+            std::cerr << e.what() << std::endl; // Kiiratjuk a hibát.
+        }
     }
 
     double getResult() const
     {
         return result;
     }
-
+    // Ez csak két segédfüggvény, ami a privateből kiolvassa a Montecalro és az ellenőrzés értékét.
     double getControl() const
     {
         return controlvalue;
     }
 
-private:
+private: // Montecarlo módszert megvalósító függvény
     template <typename Func, typename Dom>
-    double integrate(Func integrand, Dom domain, int samples)
+    double integrate(Func integrand, Dom domain, int samples) // megadjuk az integrandust, a tartományt és az ismétlés számot (nem szükséges alapértelmezetten 500 ezer)
     {
-        int indomain = 0;
         double sum = 0.0;
         for (int i = 0; i < samples; ++i)
         {
-            double x = distributionx(gen);
+            double x = distributionx(gen); // Generálunk random koordinátákat
             double y = distributiony(gen);
             double z = distributionz(gen);
 
-            if (domain(x, y, z))
+            if (domain(x, y, z)) // ellenőrizzük, hogy a tartományba találhatóak-e?
             {
-                double value = integrand(x, y, z);
-                sum += value;
-                indomain++;
+                double value = integrand(x, y, z); // Amennyiben igen, kiszámoljuk a integrandus értékét az adott ponton
+                sum += value;                      // A tartományban található pontok esetén számolt értékeket összegezzük
             }
         }
 
         double volume = (distributionx.max() - distributionx.min()) * (distributiony.max() - distributiony.min()) * (distributionz.max() - distributionz.min());
-        /*double integral = static_cast<double>(indomain) / static_cast<double>(samples) * volume;*/
-        std::cout << "Montecarlo sum:" << sum << std::endl; // tesztelés
-
-        double integral = sum / samples / 1000 * indomain * (1e6 / samples); // 1000-es szorzó miért?
+        double integral = sum / static_cast<double>(samples) * volume; // Az integrál értékének kiszámítása
         return integral;
     }
-    template <typename Func>
-    double control(Func integrand)
+    template <typename Func, typename Dom>
+    double control(Func integrand, Dom domain) // Kontrol integrálás
     {
         double a = distributionx.min();
-        double b = distributionx.max();
+        double b = distributionx.max(); // Definiáljuk azt a téglatestet ahol végig kell futni az integrálásnak
         double c = distributiony.min();
         double d = distributiony.max();
         double e = distributionz.min();
@@ -76,7 +83,7 @@ private:
         double m = (f - e) / n;
 
         double sum = 0.0;
-        for (int i = 0; i <= n; ++i)
+        for (int i = 0; i <= n; ++i) // mindegyik oldal mentén végigfutunk, a h,k,m hosszokat lépkedve
         {
             double x = a + i * h;
             for (int j = 0; j <= n; ++j)
@@ -85,7 +92,7 @@ private:
                 for (int l = 0; l <= n; ++l)
                 {
                     double z = e + l * m;
-                    if (x * x + y * y + z * z < 16.0)
+                    if (domain(x, y, z)) // erre azért volt szükség, hogy tényleg a tartmonányon integráljunk és ne az egész téglalapon.
                     {
                         double value = integrand(x, y, z);
                         sum += value;
@@ -95,8 +102,7 @@ private:
         }
 
         double volume = (b - a) * (d - c) * (f - e);
-        double integral = sum * (h * k * m);
-        std::cout << "Hagyományos sum:" << sum << std::endl; // tesztelés
+        double integral = sum * (h * k * m); // Az ellenőrző integrál meghatározása.
         return integral;
     }
 };
--- project-final/result.cpp
+++ project-final/result.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "montecarlo.hpp"
-#include <cmath>
 
 int main()
 {
@@ -9,23 +8,13 @@ int main()
         { return std::exp(-x * x - y * y - z * z); },
         [](double x, double y, double z) -> bool
         { return x * x + y * y + z * z < 16.0; },
-        -4.0, 4.0, -4.0, 4.0, -4.0, 4.0);
+        -4.0, 4.0, -4.0, 4.0, -4.0, 4.0, 2e6); // Megadjuk a változókat,amikkel a MonteCarlo integrálást végezzük.
 
-    double result = params.getResult();
+    double result = params.getResult(); // ezzel a segédfüggvénnyel előhívjuk a class, private részéből az inegrálás eredményét.
     std::cout << "\n";
 
-    /* std::cout << "Az integrálás eredménye: " << result << std::endl;
-     std::cout << "\n";
-    */
-
-    double control = params.getControl();
-    /* std::cout << "Az ellenőrzés eredménye: " << control << std::endl;
+    std::cout << "Az integrálás eredménye: " << result << std::endl; // Kiiratjuk
     std::cout << "\n";
-    */
-    /* double diff = pow(control - result, 2);
-     std::cout << "A két integrálás négyzetes eltérése:" << diff << std ::endl;
-   */
-    double percent = 100 * std::abs((result - control) / control);
-    std::cout << "A két integrálás százalákos eltérése:" << percent << std ::endl;
+
     return 0;
 }
